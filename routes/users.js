c353f065157c754c5ae99cc156b6875c207a4d16
angel <angel.wei101@gmail.com>
2022-02-14 14:48:25 -0800

    adding user validations

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const db = require('../db/models');
@@ -16,27 +17,64 @@ router.get('/register', csrfProtection, (req, res) => {
 });
 
 const userValidators = [
-  // TODO Define the user validators.
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Username')
+    .isLength({ max: 20 })
+    .withMessage('First Name must not be more than 20 characters long'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 255 })
+    .withMessage('Email Address must not be more than 255 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 50 })
+    .withMessage('Password must not be more than 50 characters long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+    .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ max: 50 })
+    .withMessage('Confirm Password must not be more than 50 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    })
 ];
 
 router.post('/register', csrfProtection, userValidators,
   asyncHandler(async (req, res) => {
     const {
-      emailAddress,
-      firstName,
-      lastName,
+      username,
+      email,
       password,
     } = req.body;
 
     const user = db.User.build({
-      emailAddress,
-      firstName,
-      lastName,
+      username,
+      email,
     });
 
     const validatorErrors = validationResult(req);
 
-    if (validatorErrors.isEmpty()) {
+    if (validatorErrors.isEmpty()) { //if no errors
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.hashedPassword = hashedPassword;
       await user.save();
       res.redirect('/');
     } else {
