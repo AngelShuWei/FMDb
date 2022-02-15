81fdbd02985f11854658300678b50fd7ab69312f
angel <angel.wei101@gmail.com>
2022-02-15 10:23:52 -0800

    created movies page

--- routes/movie.js
+++ routes/movie.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const db = require('../db/models');
 const { csrfProtection, asyncHandler } = require('./utils');
 
 const router = express.Router();
 
+router.get('/', async (req, res, next) => {
+  try {
+    const movies = await db.Movie.findAll({ order: [['name', 'ASC']] });
+    res.render('movie-list', { title: 'Movies', movies });
+  } catch (err) {
+    next(err);
+  }
+});
 
 
 
