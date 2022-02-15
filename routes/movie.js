f68ae3d43a2268c4fb26e210ada023321a439f52
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-02-15 12:36:38 -0800

    updated error message in movie.js

--- routes/movie.js
+++ routes/movie.js
@@ -5,17 +5,12 @@ const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-// const movieNotFoundError = (req, res, next) => {
-//   const movieNotFoundError = validationResult(req);
-//       if (!movieNotFoundError.isEmpty()){
-
-//           const err = Error("Movie Not Found");
-//           err.status = 404;
-//           err.title = "Movie Not found";
-//           return next(err);
-//       }
-//       next();
-// }
+const movieNotFoundError = (req, res, next) => {
+  const error = Error("Movie Not Found");
+  error.status = 404;
+  const message = "Movie Not found";
+  res.render('error', {error, message});
+}
 // const movieNotFoundError = movieId => {
 //   const err = Error("Movie could not be found.");
 //   err.title = "Movie not found.";
@@ -30,15 +25,14 @@ router.get('/', asyncHandler(async (req, res, next) => {
 
 router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
   const id = parseInt(req.params.id, 10);
-    const movie = await db.Movie.findByPk(id);
-    res.render('movie', { title: movie.name });
-    // if (movie) {
-    //   res.render('movie', { title: movie.name });
-    // } else {
-    //   next(movieNotFoundError(id));
-    // }
-}));
+  const movie = await db.Movie.findByPk(id);
 
+  if (movie) {
+    res.render('movie', { title: movie.name });
+  } else {
+    next(movieNotFoundError(req, res, next));
+  }
+}));
 
 
 
