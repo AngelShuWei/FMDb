d50a52f9a4604981786c19386c74199c781d6f56
angel <angel.wei101@gmail.com>
2022-02-16 10:42:17 -0800

    creating new collection

--- routes/collection.js
+++ routes/collection.js
@@ -6,24 +6,26 @@ const { check, validationResult } = require('express-validator');
 const router = express.Router();
 const { loginUser, logoutUser } = require('../auth');
 
-const movieNotFoundError = (req, res, next) => {
-  const error = Error("Movie Not Found");
-  error.status = 404;
-  const message = "Movie Not found";
-  res.render('error', {error, message});
-}
-// const movieNotFoundError = movieId => {
-//   const err = Error("Movie could not be found.");
-//   err.title = "Movie not found.";
-//   err.status = 404;
-//   return err;
-// }
 
 router.get('/', asyncHandler(async (req, res, next) => {
-  // const movies = await db.Movie.findAll({ order: [['name', 'ASC']] });
-
-  // res.render('collection-list', { title: 'Movies', movies});
+  res.render('collection-list', { title: "My Collections"});
 }));
+
+const collectionValidators = [
+  check('name')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for your Collection')
+    .isLength({ max: 50 })
+    .withMessage('The Collection name must not be more than 50 characters long')
+    .custom((value) => {
+      return db.Collection.findOne({ where: { name: value } })
+        .then((collection) => {
+          if (collection) {
+            return Promise.reject('The Collection name already exists');
+          }
+        });
+    }),
+];
 
 router.get('/add', csrfProtection, (req, res) => {
   const collection = db.Collection.build();
@@ -34,35 +36,39 @@ router.get('/add', csrfProtection, (req, res) => {
   });
 });
 
-router.post('/add', csrfProtection, asyncHandler(async (req, res) => {
-  const {
-    title,
-    author,
-    releaseDate,
-    pageCount,
-    publisher,
-  } = req.body;
+router.post('/add', csrfProtection, collectionValidators, asyncHandler(async (req, res) => {
+  // const id = req.session.auth.userId;
+  // console.log(id);
 
-  const book = db.Book.build({
-    title,
-    author,
-    releaseDate,
-    pageCount,
-    publisher,
+  const { name, userId } = req.body;
+
+  const collection = db.Collection.build({
+    name
   });
 
-  try {
-    await book.save();
-    res.redirect('/');
-  } catch (err) {
-    res.render('book-add', {
-      title: 'Add Book',
-      book,
-      error: err,
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) { //if no errors
+    await collection.save();
+    res.redirect('/collections');
+  } else {
+    const errors = validatorErrors.array().map((error) => error.msg);
+    res.render('add-collection', {
+      title: 'Create New Collection',
+      collection,
+      errors,
       csrfToken: req.csrfToken(),
     });
   }
-}));
+  }));
+
+
+// const loginUser = (req, res, user) => {
+//   req.session.auth = {
+//     userId: user.id,
+//   };
+// };
+
 
 // router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
 //   const id = parseInt(req.params.id, 10);
