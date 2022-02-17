4957f63fb630fdde4928e570f32cd2dcc6afd971
angel <angel.wei101@gmail.com>
2022-02-16 21:31:22 -0800

    fixed it so each user can only have one collection name

--- routes/collection.js
+++ routes/collection.js
@@ -35,14 +35,17 @@ const collectionValidators = [
     .withMessage('Please provide a value for your Collection')
     .isLength({ max: 50 })
     .withMessage('The Collection name must not be more than 50 characters long')
-    .custom((value) => {
-      return db.Collection.findOne({ where: { name: value } })
-        .then((collection) => {
-          if (collection) {
-            return Promise.reject('The Collection name already exists');
-          }
-        });
-    }),
+  // .custom((value) => { //value is whatever the user passed in. It gets fed into the chain
+  //   return db.Collection.findOne({ where: {
+  //     name: value,
+  //     userId: req.session.auth.userId //means that for EACH user, needs to have unique collection name
+  //    }})
+  //     .then((collection) => { //if true, then will reject that request
+  //       if (collection) {
+  //         return Promise.reject('The Collection name already exists');
+  //       }
+  //     });
+  // }),
 ];
 
 
@@ -72,11 +75,30 @@ router.post('/add', csrfProtection, collectionValidators, asyncHandler(async (re
 
   const validatorErrors = validationResult(req);
 
-  if (validatorErrors.isEmpty()) { //if no errors
-    await collection.save();
+  const errors = validatorErrors.array().map(error => error.msg); //map through the validation results. Are there errors?
+  const collectionExists = await db.Collection.findOne({
+    where: {
+      name, //coming into the body as name
+      userId: parseInt(userId, 10) //means that for EACH user, needs to have unique collection name. If invalid integer type, and references dialect/postgres/queryy means the query is incorrect
+    }
+  })
+
+  if (collectionExists) {
+    errors.push('The Collection name already exists')
+  }
+
+  if (!errors.length) { //there is NO length in the errors arary meaning it's GOOOOOOD SHIT
+    await collection.save(); //dont want to save until know that there isn't an error
     res.redirect('/collections');
   } else {
-    const errors = validatorErrors.array().map((error) => error.msg);
+    // const errors = validatorErrors.array().map((error) => error.msg);
+    // const collectionExists = await db.Collection.findOne({ where: {
+    //   name, //coming into the body as name
+    //   userId //means that for EACH user, needs to have unique collection name
+    // }})
+    // if (collectionExists) {
+    //   errors.push('The Collection name already exists')
+    // }
     res.render('add-collection', {
       title: 'Create New Collection',
       collection,
@@ -95,7 +117,7 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
   const collection = await db.Collection.findByPk(id);
   const collectionId = id
 
-  const collectionMovies = await db.CollectionMovie.findAll({ where: {collectionId}})
+  const collectionMovies = await db.CollectionMovie.findAll({ where: { collectionId } })
 
   let movieIds = [];
   collectionMovies.forEach(movie => {
@@ -125,50 +147,41 @@ router.get('/test', csrfProtection, (req, res) => {
 
 
 router.post('/add-movie', csrfProtection, asyncHandler(async (req, res) => {
-    const userId = req.session.auth.userId;
-    // const collections = await db.Collection.findAll({ where: { userId } });
-    // const collection = await db.Collection.findByPk(id);
-    const {
-      addToCollections
-    } = req.body;
+  const userId = req.session.auth.userId;
 
-    const body = addToCollections.split('#');
-    const collectionName = body[0];
-    const movieIdStr = body[1];
-    const movieId = parseInt(movieIdStr, 10);
+  const {
+    addToCollections
+  } = req.body;
 
-    const collectionObj = await db.Collection.findOne({
-      where: {
-        userId,
-        name: collectionName
-       }
-    })
+  const body = addToCollections.split('#');
+  const collectionIdStr = body[0];
+  const movieIdStr = body[1];
+  const collectionId = parseInt(collectionIdStr, 10);
+  const movieId = parseInt(movieIdStr, 10);
 
-    const collectionId = collectionObj.id;
-
-    const tableExists = await db.CollectionMovie.findOne({
-      where: {
-        movieId,
-        collectionId
-      }
-    })
-
-    if (!tableExists) {
-      const collectionMovie = db.CollectionMovie.build({
-        movieId,
-        collectionId
-      });
-
-      await collectionMovie.save();
-      res.redirect(`/collections/${collectionId}`)
-      // res.redirect('/movies/1')
-    } else {
-      //TO DO
-      //RE RENDER PAGE WITH ERROR BECAUSE JOIN TABLE EXISTS ALREADY
-      res.send('Cannot add same movie into this collection');
+  const tableExists = await db.CollectionMovie.findOne({
+    where: {
+      movieId,
+      collectionId
     }
-
   })
+
+  if (!tableExists) {
+    const collectionMovie = db.CollectionMovie.build({
+      movieId,
+      collectionId
+    });
+
+    await collectionMovie.save();
+    res.redirect(`/collections/${collectionId}`)
+    // res.redirect('/movies/1')
+  } else {
+    //TO DO
+    //RE RENDER PAGE WITH ERROR BECAUSE JOIN TABLE EXISTS ALREADY
+    res.send('Cannot add same movie into this collection');
+  }
+
+})
 );
 
 
