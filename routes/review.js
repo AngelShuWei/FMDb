7da06d4c089ea24faf27f663f9df6e9254b3794b
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-02-17 12:31:39 -0800

    added review form

--- routes/review.js
+++ routes/review.js
@@ -7,6 +7,13 @@ const { csrfProtection, asyncHandler } = require('./utils');
 
 const router = express.Router();
 
+const logIntoReviewError = (req, res, next) => {
+    const error = Error("Not Logged In");
+    error.status = 404;
+    const message = "Please log in or register to post a review :)";
+    res.render('error', { error, message });
+};
+
 
 router.get('/', csrfProtection, asyncHandler( async (req, res) => {
     const id = parseInt(req.params.id, 10);
@@ -16,6 +23,20 @@ router.get('/', csrfProtection, asyncHandler( async (req, res) => {
     res.render('review-list', { reviews })
 }));
 
+router.get('/add', csrfProtection, asyncHandler(async (req, res) => {
+    if (req.session.auth) {
+        const userId = req.session.auth.userId;
+        const review = db.Review.build();
+        res.render('review-form', {
+            title: 'Add Review',
+            review,
+            userId,
+            csrfToken: req.csrfToken(),
+        });
+    } else {
+        next(logIntoReviewError(req, res, next));
+    };
+}));
 
 
 
