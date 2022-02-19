67d0eb612a408a6a423e2861ac2f3072379a54b6
cc-y-zhao <cecilia_zhao@berkeley.edu>
2022-02-18 23:53:59 -0800

    changed landing page to render movie-list.pug and changed logo to redirect to /movies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Featured today' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const movies = await db.Movie.findAll({ order: [['name', 'ASC']] });
+  res.render('movie-list', { title: 'Movies', movies });
+}));
 
 module.exports = router;
